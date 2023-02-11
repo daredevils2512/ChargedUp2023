2654b665114e0f1328464b6add14ba131feac6f9
NOAH <smurfmuchies@gmail.com>
2023-02-11 10:00:26 -0600

    fix right and left on controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,6 +2,9 @@ package frc.robot;
 
 import frc.robot.subsystems.DriveSub;
 import frc.robot.utils.Constants.IoConstants;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -13,7 +16,8 @@ public class RobotContainer {
    private final CommandXboxController m_driverController =
   new CommandXboxController(IoConstants.XBOX_CONTROLLER_PORT);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /** The container for the ro
+   * bot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
@@ -29,7 +33,7 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    driveSub.setDefaultCommand(driveSub.run(() -> driveSub.arcadeDrive(m_driverController.getLeftX(), m_driverController.getRightY())));
+    driveSub.setDefaultCommand(driveSub.run(() -> driveSub.arcadeDrive(-m_driverController.getRightX(), m_driverController.getLeftY())));
   }
 
   /**
