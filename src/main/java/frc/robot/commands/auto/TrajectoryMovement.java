be8a25eae568e52fb4fa1b7d901cf52ae7814e66
A-Myr <amir.teimur@gmail.com>
2023-02-15 16:20:31 -0600

    Added an error tolerance thing for other command

--- src/main/java/frc/robot/commands/auto/TrajectoryMovement.java
+++ src/main/java/frc/robot/commands/auto/TrajectoryMovement.java
@@ -1,10 +1,14 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import frc.robot.subsystems.AutoDriveSub;
+import frc.robot.utils.Constants.Auto;
 import frc.robot.utils.Constants.AutoDriveConstants;
 
 public class TrajectoryMovement extends CommandBase {
@@ -19,6 +23,11 @@ public class TrajectoryMovement extends CommandBase {
         auto_drive = drivetrain;
         path = trajectory;
         ramseteControl = new RamseteController();
+
+        Rotation2d rotationTolerance = new Rotation2d(Auto.AUTO__DEGREES_ERROR * Math.PI / 180);
+        Translation2d transTolerance = new Translation2d(Auto.AUTO_ERROR, Auto.AUTO_ERROR);
+        Pose2d poseTolerance = new Pose2d(transTolerance, rotationTolerance);
+        ramseteControl.setTolerance(poseTolerance);    
 
         addRequirements(drivetrain);
     }
